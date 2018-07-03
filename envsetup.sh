346dd2a2beb6e130291d8d5fa0196fc03eb3ea8d
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-07-03 08:47:11 +0200

    env: add repopick trigger
    Change-Id: Ib49d9450b935deb72bca602a3f33ee43742f1b46
    (cherry picked from commit fbe7618184a6ff6ecbc49eee976b7b43055f6c4f)
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- envsetup.sh
+++ envsetup.sh
@@ -2077,3 +2077,9 @@ set_global_paths
 source_vendorsetup
 addcompletions
 
+export ANDROID_BUILD_TOP=$(gettop)
+
+function repopick() {
+    T=$(gettop)
+    $T/vendor/droidx/build/tools/repopick.py $@
+}
