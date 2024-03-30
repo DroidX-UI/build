19b71665b7efae0971e70be1624d193e27db050f
AgentFabulous <kshitijgm@gmail.com>
2024-03-30 11:32:52 +0000

    build: Add DroidX-UI ASCII art on lunch
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -576,6 +576,10 @@ function _lunch_meat()
     if [[ -n "${CHECK_MU_CONFIG:-}" ]]; then
       check_mu_config
     fi
+
+    echo "";
+    cat $(gettop)/build/make/droidx_ascii_logo;
+    echo"";
 }
 
 unset COMMON_LUNCH_CHOICES_CACHE
