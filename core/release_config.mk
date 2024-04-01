f1b0c599791f7dba89655bf36be3da54306710a9
Stella Bloom <windowz414@gnuweeb.org>
2024-04-01 10:06:59 +0000

    Fix release configuration in build system once and for all.
    Some autocompleting shells such as ble.sh cause problems when
    typing out the command to manually lunch (I.e. we get lots of
    invalid TARGET_RELEASE errors just as we type "lunch aosp_ar" for
    autocomplete to suggest "lunch aosp_arm-ap2a-eng"). These problems
    cause "lunch" to malfunction and render it unable to provide
    autocompletion suggestions.
    
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- core/release_config.mk
+++ core/release_config.mk
@@ -275,7 +275,7 @@ ifeq ($(TARGET_RELEASE),)
     # Instead of leaving this string empty, we want to default to a valid
     # setting.  Full builds coming through this path is a bug, but in case
     # of such a bug, we want to at least get consistent, valid results.
-    TARGET_RELEASE = trunk_staging
+    TARGET_RELEASE = ap4a
 endif
 
 # During pass 1 of product config, using a non-existent release config is not an error.
