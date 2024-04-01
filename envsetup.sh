2efae5395d93178cd6c4cb46a2ead317d88c3966
Stella Bloom <windowz414@gnuweeb.org>
2024-04-01 10:06:59 +0000

    Fix release configuration in build system once and for all.
    Some autocompleting shells such as ble.sh cause problems when
    typing out the command to manually lunch (I.e. we get lots of
    invalid TARGET_RELEASE errors just as we type "lunch aosp_ar" for
    autocomplete to suggest "lunch aosp_arm-ap2a-eng"). These problems
    cause "lunch" to malfunction and render it unable to provide
    autocompletion suggestions.
    
    Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -791,8 +791,8 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo "Which would you like? [aosp_cf_x86_64_phone-trunk_staging-eng]"
-        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-trunk_staging-eng): "
+        echo "Which would you like? [aosp_cf_x86_64_phone-ap2a-eng]"
+        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-ap2a-eng): "
         read answer
         used_lunch_menu=1
     fi
@@ -801,7 +801,7 @@ function lunch()
 
     if [ -z "$answer" ]
     then
-        selection=aosp_cf_x86_64_phone-trunk_staging-eng
+        selection=aosp_cf_x86_64_phone-ap2a-eng
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
         local choices=($(TARGET_BUILD_APPS= TARGET_PRODUCT= TARGET_RELEASE= TARGET_BUILD_VARIANT= get_build_var COMMON_LUNCH_CHOICES 2>/dev/null))
@@ -927,7 +927,7 @@ function tapas()
     local showHelp="$(echo $* | xargs -n 1 echo | \grep -E '^(help)$' | xargs)"
     local arch="$(echo $* | xargs -n 1 echo | \grep -E '^(arm|x86|arm64|x86_64)$' | xargs)"
     # TODO(b/307975293): Expand tapas to take release arguments (and update hmm() usage).
-    local release="trunk_staging"
+    local release="ap2a"
     local variant="$(echo $* | xargs -n 1 echo | \grep -E '^(user|userdebug|eng)$' | xargs)"
     local density="$(echo $* | xargs -n 1 echo | \grep -E '^(ldpi|mdpi|tvdpi|hdpi|xhdpi|xxhdpi|xxxhdpi|alldpi)$' | xargs)"
     local keys="$(echo $* | xargs -n 1 echo | \grep -E '^(devkeys)$' | xargs)"
@@ -1000,7 +1000,7 @@ function banchan()
     local showHelp="$(echo $* | xargs -n 1 echo | \grep -E '^(help)$' | xargs)"
     local product="$(echo $* | xargs -n 1 echo | \grep -E '^(.*_)?(arm|x86|arm64|riscv64|x86_64|arm64only|x86_64only)$' | xargs)"
     # TODO: Expand banchan to take release arguments (and update hmm() usage).
-    local release="trunk_staging"
+    local release="ap2a"
     local variant="$(echo $* | xargs -n 1 echo | \grep -E '^(user|userdebug|eng)$' | xargs)"
     local apps="$(echo $* | xargs -n 1 echo | \grep -E -v '^(user|userdebug|eng|(.*_)?(arm|x86|arm64|riscv64|x86_64))$' | xargs)"
 
