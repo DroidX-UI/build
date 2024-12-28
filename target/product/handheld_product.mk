573c3405bd8164c15eeb59337207b4e84a35dfd6
Nalinish Ranjan <nalinishranjan05@gmail.com>
2024-12-28 16:30:03 +0530

    Exclude Browser2 and QuickSearchBox from DroidX-UI Builds

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -22,21 +22,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
-    Browser2 \
     Calendar \
     Contacts \
     DeskClock \
     Gallery2 \
     Music \
     preinstalled-packages-platform-handheld-product.xml \
-    QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
 
 ifeq ($(DROIDX_BUILD),)
 PRODUCT_PACKAGES += \
+    Browser2 \
     Camera2 \
-    LatinIME
+    LatinIME \
+    QuickSearchBox
 endif
 
 PRODUCT_PACKAGES_DEBUG += \
